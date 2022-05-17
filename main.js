0e8218eb14047f4217f21cdff3b008158dd4a772
ggn1 <ggn1@hw.ac.uk>
2022-05-18 00:05:51 +0400

    updated 3.js learning

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-// Define scene.
-const scene = new THREE.Scene();
-
-// Define camera.
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
-
 // Define renderer.
 const renderer = new THREE.WebGLRenderer({antialias:true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 $('body').append(renderer.domElement); // Add renderer to webpage.
 
-// Create geometry.
-const geometry = new THREE.BoxGeometry(1,1,1); // Equal width, height, depth.
-const material = new THREE.MeshBasicMaterial({color:0xff0000});
-const cube = new THREE.Mesh(geometry, material);
+// Define scene.
+const scene = new THREE.Scene();
 
-// Add geometry to scene.
+// Make scene have white background.
+// scene.fog = new THREE.Fog(0xffffff, 0, 2000);
+renderer.setClearColor(0xffffff, 1);
+
+// Define camera.
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
+
+// Add geometry.
+const geometry = new THREE.BoxGeometry(1,1,1); // Equal width, height, depth.
+const material = new THREE.MeshPhongMaterial({color:0xff0000});
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+// Add light.
+const directional_light = new THREE.DirectionalLight(0xffffff, 1.2);
+directional_light.position.x = 3;
+directional_light.position.y = 4;
+directional_light.position.z = 4;
+scene.add(directional_light);
 
 // Rotate cube about axis.
 cube.position.z = -5;
