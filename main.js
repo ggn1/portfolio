39bae151f0c881a110d1cdb2ba7cb839f5d70f2e
ggn1 <ggn1@hw.ac.uk>
2022-05-18 00:35:56 +0400

    uploaded a three.js object from blender

--- main.js
+++ main.js
@@ -13,25 +13,32 @@ renderer.setClearColor(0xffffff, 1);
 // Define camera.
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight);
 
+// Create loader to load in blender object.
+const loader = new THREE.GLTFLoader();
+
 // Add geometry.
-const geometry = new THREE.BoxGeometry(1,1,1); // Equal width, height, depth.
-const material = new THREE.MeshPhongMaterial({color:0xff0000});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const geometry = new THREE.BoxGeometry(1,1,1); // Equal width, height, depth.
+// const material = new THREE.MeshPhongMaterial({color:0xff0000});
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
+var chess_queen;
+loader.load('chess_queen.glb', (gltf) => {
+    chess_queen = gltf.scene;
+    scene.add(chess_queen);
+    chess_queen.position.z = -10;
+    animate(chess_queen);
+})
+
+let animate = () => {
+    // Rotate object about axis.
+    chess_queen.rotation.y += 0.01;
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+};
 
 // Add light.
-const directional_light = new THREE.DirectionalLight(0xffffff, 1.2);
+const directional_light = new THREE.DirectionalLight(0xffffff, 2.0);
 directional_light.position.x = 3;
 directional_light.position.y = 4;
 directional_light.position.z = 4;
 scene.add(directional_light);
-
-// Rotate cube about axis.
-cube.position.z = -5;
-const animate = () => {
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-};
-animate();
