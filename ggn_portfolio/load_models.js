c77914df6f8c59ca4a8a3cedeeb8a98b59654dbb
ggn1 <ggn1@hw.ac.uk>
2022-06-26 21:15:20 +0530

    added objects

--- ggn_portfolio/load_models.js
+++ ggn_portfolio/load_models.js
@@ -9,12 +9,17 @@ const set_up_model = (model_data) => {
 }
 
 async function load_models() {
-    const human_data = await loader.loadAsync('./assets/human.glb');
-    const heart_data = await loader.loadAsync('./assets/heart.glb');
-    const phone_data = await loader.loadAsync('./assets/phone.glb');
-    const gear_big_data = await loader.loadAsync('./assets/gear_big.glb');
-    const gear_medium_data = await loader.loadAsync('./assets/gear_medium.glb');
-    const gear_small_data = await loader.loadAsync('./assets/gear_small.glb');
+    const [
+        human_data, heart_data, phone_data, 
+        gear_big_data, gear_medium_data, gear_small_data
+    ] = await Promise.all([
+        loader.loadAsync('./assets/human.glb'),
+        loader.loadAsync('./assets/heart.glb'),
+        loader.loadAsync('./assets/phone.glb'),
+        loader.loadAsync('./assets/gear_big.glb'),
+        loader.loadAsync('./assets/gear_medium.glb'),
+        loader.loadAsync('./assets/gear_small.glb')
+    ]);
 
     const human = set_up_model(human_data);
 
@@ -30,22 +35,11 @@ async function load_models() {
         small: set_up_model(gear_small_data)
     }
 
+    gears.big.position.set(0, 3.2, -0.5);
+    gears.medium.position.set(1.5, 4, -0.7);
+    gears.small.position.set(-1.45, 4, -0.7);
+
     return { human, heart, phone, gears};
 }
-
-// async function load_character() {
-//     
-//     loader.load('assets/character.gltf', (gltf) => {
-//         model_character = gltf.scene;
-//         scene.add(model_character);
-//         model_character.scale.set(5, 5, 5);
-//         model_character.rotation.set(0, 3.1, 0);
-//         model_character.position.set(0, -10, 0);
-//     }, (xhr) => {
-//         console.log("character:", xhr.loaded/xhr.total * 100, "% loaded");
-//     }, (error) => {
-//         console.log("oops! error occurred in loading character");
-//     });
-// }
 
 export { load_models };
--- ggn_portfolio/main.js
+++ ggn_portfolio/main.js
@@ -32,95 +32,11 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 // renderer.setClearColor(0x000000, 0.9);
-
-// // Set up asset gltf_loader.
-// const gltf_loader = new GLTFLoader();
-
-// Load objects.
-
-// // Object 1. Load character.
-// let model_character;
-// gltf_loader.load('assets/character.gltf', (gltf) => {
-//   model_character = gltf.scene;
-//   scene.add(model_character);
-//   model_character.scale.set(5, 5, 5);
-//   model_character.rotation.set(0, 3.1, 0);
-//   model_character.position.set(0, -10, 0);
-// }, (xhr) => {
-//   console.log("character:", xhr.loaded/xhr.total * 100, "% loaded");
-// }, (error) => {
-//   console.log("oops! error occurred in loading character");
-// });
-
-// // Object 2. Load heart.
-// let model_heart;
-// gltf_loader.load('assets/heart.gltf', (gltf) => {
-//   model_heart = gltf.scene;
-//   scene.add(model_heart);
-//   model_heart.scale.set(5, 5, 5);
-//   model_heart.rotation.set(0, 3.1, 0);
-//   model_heart.position.set(7, 5.5, 1);
-// }, (xhr) => {
-//   console.log("heart:", xhr.loaded/xhr.total * 100, "% loaded");
-// }, (error) => {
-//   console.log("oops! error occurred in loading heart");
-// });
-
-// // Object 3. Load phone.
-// let model_phone;
-// gltf_loader.load('assets/phone.gltf', (gltf) => {
-//   model_phone = gltf.scene;
-//   scene.add(model_phone);
-//   model_phone.scale.set(5, 5, 5);
-//   model_phone.rotation.set(0, 3.1, 0);
-//   model_phone.position.set(-7, 5.5, 1);
-// }, (xhr) => {
-//   console.log("phone:", xhr.loaded/xhr.total * 100, "% loaded");
-// }, (error) => {
-//   console.log("oops! error occurred in loading phone");
-// });
-
-// // Object 4. Load gear_big.
-// let model_gear_big;
-// gltf_loader.load('assets/gear_big.gltf', (gltf) => {
-//   model_gear_big = gltf.scene;
-//   scene.add(model_gear_big);
-//   model_gear_big.scale.set(5, 5, 5);
-//   model_gear_big.rotation.set(0, 3.1, 0);
-//   model_gear_big.position.set(0, 7, -1.5);
-// }, (xhr) => {
-//   console.log("gear_big:", xhr.loaded/xhr.total * 100, "% loaded");
-// }, (error) => {
-//   console.log("oops! error occurred in loading gear_big");
-// });
-
-// // Object 5. Load gear_medium.
-// let model_gear_medium;
-// gltf_loader.load('assets/gear_medium.gltf', (gltf) => {
-//   model_gear_medium = gltf.scene;
-//   scene.add(model_gear_medium);
-//   model_gear_medium.scale.set(5, 5, 5);
-//   model_gear_medium.rotation.set(0, 3.1, 0);
-//   model_gear_medium.position.set(-8, 11, -2.5);
-// }, (xhr) => {
-//   console.log("gear_medium:", xhr.loaded/xhr.total * 100, "% loaded");
-// }, (error) => {
-//   console.log("oops! error occurred in loading gear_medium");
-// });
-
-// // Object 6. Load gear_small.
-// let model_gear_small;
-// gltf_loader.load('assets/gear_small.gltf', (gltf) => {
-//   model_gear_small = gltf.scene;
-//   scene.add(model_gear_small);
-//   model_gear_small.scale.set(5, 5, 5);
-//   model_gear_small.rotation.set(0, 3.1, 0);
-//   model_gear_small.position.set(8, 10, -2.5);
-// }, (xhr) => {
-//   console.log("gear_small:", xhr.loaded/xhr.total * 100, "% loaded");
-// }, (error) => {
-//   console.log("oops! error occurred in loading gear_small");
-// });
+window.onresize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth/window.innerHeight;
+  camera.updateProjectionMatrix();
+};
 
 // Add lighting.
 const light = new THREE.DirectionalLight(0xffffff, 0.8);
@@ -145,7 +61,7 @@ init();
 // Render objects on screen.
 // let float_val;
 const animate = () => {
-//   requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
 //   // float character
 //   if (model_character.position.y >= -10) float_val = -0.002;
