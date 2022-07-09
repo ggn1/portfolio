4b04e88a468ef022d951f80deb4e2da8510877ab
ggn1 <ggn1@hw.ac.uk>
2022-07-09 14:57:52 +0400

    added basic header

---
+++ ggn_portfolio_new/portfolio/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../src/index.css"
+export default function Header() {
+  return (
+    <div className="code" style={{padding:'1%', color:"white", position: 'fixed'}}>
+        C:\Users\<b><i>Gayathri Girish Nair</i></b>{">"} Hello World! :)
+    </div>
+  )
+}
