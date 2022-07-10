4809a9855ec31abb1abfd24349621c8efcaaa2dc
ggn1 <ggn1@hw.ac.uk>
2022-07-10 18:32:23 +0400

    loaded static models

--- ggn_portfolio_new/portfolio/components/Threejs.jsx
+++ ggn_portfolio_new/portfolio/components/Threejs.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect } from 'react';
-import "./Model3d.css";
+import "./Threejs.css";
 import * as THREE from 'three';
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 export default function Model3d() {
 
@@ -43,6 +44,56 @@ export default function Model3d() {
     controls.enablePan = false;
     controls.minDistance = 10;
     controls.maxDistance = 10;
+
+    // load objects
+    const loader = new GLTFLoader();
+
+    const set_up_model = (model_data) => {
+      const model = model_data.scene;
+      model.rotation.set(0, 3.14, 0); // rotate z axis by 180 degrees
+      return model;
+    }
+
+    const load_models = async () => {
+        const [
+            human_data, heart_data, phone_data, 
+            gear_big_data, gear_medium_data, gear_small_data
+        ] = await Promise.all([
+            loader.loadAsync('../assets/human.glb'),
+            loader.loadAsync('../assets/heart.glb'),
+            loader.loadAsync('../assets/phone.glb'),
+            loader.loadAsync('../assets/gear_big.glb'),
+            loader.loadAsync('../assets/gear_medium.glb'),
+            loader.loadAsync('../assets/gear_small.glb')
+        ]);
+    
+        const human = set_up_model(human_data);
+    
+        const heart = set_up_model(heart_data);
+        heart.position.set(1.3, 3.1, 0.3);
+    
+        const phone = set_up_model(phone_data);
+        phone.position.set(-1.3, 3.1, 0.3);
+    
+        const gears = {
+            big: set_up_model(gear_big_data),
+            medium: set_up_model(gear_medium_data),
+            small: set_up_model(gear_small_data)
+        }
+    
+        gears.big.position.set(0, 3.2, -0.5);
+        gears.medium.position.set(1.5, 4, -0.7);
+        gears.small.position.set(-1.45, 4, -0.7);
+
+        scene.add(human);
+        scene.add(heart);
+        scene.add(phone);
+        scene.add(gears.big);
+        scene.add(gears.medium);
+        scene.add(gears.small);
+    }
+
+    load_models();
 
     // render on screen
     const animate = () => {
--- ggn_portfolio_new/portfolio/src/App.jsx
+++ ggn_portfolio_new/portfolio/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from "../components/Header"
-import Model3d from '../components/Model3d';
+import Threejs from '../components/Threejs';
 
 function App() {
   return (
     <>
       <Header />
-      <Model3d />
+      <Threejs />
     </>
   );
 }
