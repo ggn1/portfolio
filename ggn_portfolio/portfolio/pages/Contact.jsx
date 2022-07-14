64e234345c69b33dc47bdb14768c09617345451d
ggn1 <ggn1@hw.ac.uk>
2022-07-14 23:06:56 +0400

    redirect added

--- ggn_portfolio/portfolio/pages/Contact.jsx
+++ ggn_portfolio/portfolio/pages/Contact.jsx
@@ -5,8 +5,8 @@ import "./Contact.css"
 export default function Contact() {
   return (
     <div className="contacts">
-      <Button img_src="../assets/gmail.png" px_size={200} />
-      <Button img_src="../assets/linkedin.png" px_size={200} />
+      <Button img_src="../assets/gmail.png"/>
+      <Button img_src="../assets/linkedin.png" on_click={() => window.open('//www.linkedin.com/in/gayathrigirishnair')} />
     </div>
   )
 }
