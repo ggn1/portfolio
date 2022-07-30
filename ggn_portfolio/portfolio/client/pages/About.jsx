49a4f97f9d26b9d173dbd67ef686913666ebf718
ggn1 <ggn1@hw.ac.uk>
2022-07-31 00:51:41 +0400

    parallax failed

--- ggn_portfolio/portfolio/client/pages/About.jsx
+++ ggn_portfolio/portfolio/client/pages/About.jsx
@@ -1,13 +1,22 @@
 import React, { useEffect } from 'react'
 import AboutContent from "../components/AboutContent"
+import Parallax from "../components/Parallax"
 
 export default function About() {
+  const bg_images = <div>
+    <img className='layer' data_speed={30} src="../assets/fly_cyan.png" />
+    <img className='layer' data_speed={-30} src="../assets/fly_pink.png" />
+    <img className='layer' data_speed={-2} src="../assets/fly_yellow.png" />
+  </div>
+  
   useEffect(() => {
     document.getElementById("nav_bar").className = "no_overlay";
     return () => document.getElementById("nav_bar").className = "overlay";
   });
 
   return (
+    <>
       <AboutContent />
+    </>
   )
 }
--- ggn_portfolio/portfolio/client/pages/Home.jsx
+++ ggn_portfolio/portfolio/client/pages/Home.jsx
@@ -1,19 +1,10 @@
 import React, { useState } from 'react'
 import Threejs from '../components/Threejs'
-import Parallax from "../components/Parallax"
 
 export default function Home() {
-
-  const bg_images = <div>
-    <img className='layer' data_speed={1} src="../assets/pixel_explode_1.png" />
-    <img className='layer' data_speed={4} src="../assets/pixel_explode_2.png" />
-    <img className='layer' data_speed={8} src="../assets/pixel_explode_3.png" />
-  </div>
-
   return (
     <div id="canvas_div">
       <Threejs />
-      <Parallax event="mouseover"/>
     </div>
   )
 }
