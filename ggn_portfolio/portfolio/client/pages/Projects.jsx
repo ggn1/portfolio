e2c479d39bd79455ca3ad9a096d3f101bf11cd3f
ggn1 <ggn1@hw.ac.uk>
2022-08-03 01:01:33 +0400

    updated preloader

--- ggn_portfolio/portfolio/client/pages/Projects.jsx
+++ ggn_portfolio/portfolio/client/pages/Projects.jsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from 'react'
-import ProjectGallery from '../components/ProjectGallery';
+import ProjectGallery from '../components/ProjectGallery'
 
 export default function Projects() {
   useEffect(() => {
     document.getElementById("nav_bar").className = "no_overlay";
     return () => document.getElementById("nav_bar").className = "overlay";
   });
+  
   return (
-    <div>
-      <ProjectGallery />
-    </div>
+    <ProjectGallery />
   )
 }
